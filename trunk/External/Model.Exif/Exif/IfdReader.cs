a1bef58601e6a21edb478453160f350b5a1f9337
Stephen <Stephen@e50ce43e-6f37-11de-baec-69240fdc299a>
2006-09-25 06:38:19 +0000

    - Fixed IFD reader when data is in offset

--- trunk/External/Model.Exif/Exif/IfdReader.cs
+++ trunk/External/Model.Exif/Exif/IfdReader.cs
@@ -8,10 +8,16 @@ namespace PhotoLib.Model.Exif
 	//{
 	//    static void Main(string[] args)
 	//    {
-	//        System.Drawing.Bitmap photo = new System.Drawing.Bitmap(@"..\..\..\IMG_0048.JPG");
-
-	//        System.Drawing.Imaging.PropertyItem2 prop = photo.GetPropertyItem(/*MakerNote*/0x927c);
-	//        PropertyItem2[] makerNotes = IfdReader.DecodeIFD(prop.Value);
+	//        const string FILENAME = @"..\..\..\Tims_10D.JPG";
+	//        System.Drawing.Imaging.PropertyItem2 prop;
+	//        using (System.Drawing.Bitmap photo = new System.Drawing.Bitmap(FILENAME))
+	//        {
+	//            prop = photo.GetPropertyItem(/*MakerNote*/0x927c);
+	//        }
+	//        using (System.IO.FileStream file = new System.IO.FileStream(FILENAME, System.IO.FileMode.Open))
+	//        {
+	//            PropertyItem2[] makerNotes = IfdReader.DecodeIFD(prop.Value, file);
+	//        }
 	//    }
 	//}
 
@@ -89,11 +95,12 @@ namespace PhotoLib.Model.Exif
 		/// <returns></returns>
 		/// <remarks>
 		/// References:
+		/// http://www.ee.cooper.edu/courses/course_pages/past_courses/EE458/TIFF/
 		/// http://search.cpan.org/src/EXIFTOOL/Image-ExifTool-6.36/html/TagNames/Canon.html
 		/// http://www.burren.cx/david/canon.html
 		/// http://cpan.uwinnipeg.ca/htdocs/Image-ExifTool/Image/ExifTool/Canon.pm.html
 		/// </remarks>
-		public static PropertyItem2[] DecodeIFD(byte[] bytes)
+		public static PropertyItem2[] DecodeIFD(byte[] bytes, System.IO.FileStream fullFile)
 		{
 			int index = 0;
 			int count = (int)BitConverter.ToUInt16(bytes, index);
@@ -119,23 +126,19 @@ namespace PhotoLib.Model.Exif
 				int length = GetSizeOf(items[i].Type) * items[i].Len;
 				if (length > 4)
 				{
+
 					// read in the Data as offset (4 bytes)
 					int offset = (int)BitConverter.ToUInt32(bytes, index);
-#if DEBUG
-					try {
-#endif
-					items[i].Value = CopyBytes(bytes, offset, length);
-#if DEBUG
-					} catch {}
-#endif
-					index += UInt32_Size;
+					items[i].Value = new byte[length];//CopyBytes(bytes, offset, length);
+					fullFile.Position = offset;
+					fullFile.Read(items[i].Value, 0, length);
 				}
 				else
 				{
 					// read in the Data as byte[]
 					items[i].Value = CopyBytes(bytes, index, length);
-					index += length;
 				}
+				index += UInt32_Size;
 			}
 
 			return items;
@@ -147,9 +150,20 @@ namespace PhotoLib.Model.Exif
 
 		private static byte[] CopyBytes(byte[] bytes, int index, int length)
 		{
+#if DEBUG
+			try
+			{
+#endif
 			byte[] data = new byte[length];
 			Array.Copy(bytes, index, data, 0, length);
 			return data;
+#if DEBUG
+			}
+			catch
+			{
+				return null;
+			}
+#endif
 		}
 
 		private static int GetSizeOf(short type)
@@ -157,32 +171,32 @@ namespace PhotoLib.Model.Exif
 			switch (type)
 			{
 				case 1:/* unsigned byte */
-					return 1;
+				return 1;
 				case 2:/* ascii strings */
-					return 1;
+				return 1;
 				case 3:/* unsigned short */
-					return 2;
+				return 2;
 				case 4:/* unsigned long */
-					return 4;
+				return 4;
 				case 5:/* unsigned rational */
-					return 8;
+				return 8;
 				case 6:/* signed byte */
-					return 1;
+				return 1;
 				case 7:/* undefined */
-					return 1;
+				return 1;
 				case 8:/* signed short */
-					return 2;
+				return 2;
 				case 9:/* signed long */
-					return 4;
+				return 4;
 				case 10:/* signed rational */
-					return 8;
+				return 8;
 				case 11:/* single float */
-					return 4;
+				return 4;
 				case 12:/* double float */
-					return 8;
+				return 8;
 
 				default:
-					return 0;
+				return 0;
 			}
 		}
 
