07f5a04bd50f47f4c166f613ebd298d01b51b9c1
Stephen McKamey <mckamey@users.noreply.github.com>
2008-05-25 19:56:55 +0000

    - more EXIF namespace reorganization

--- External/ExifUtils/Exif/ExifProperty.cs
+++ External/ExifUtils/Exif/ExifProperty.cs
@@ -6,6 +6,7 @@ using System.Xml.Serialization;
 using System.Drawing;
 using System.Drawing.Imaging;
 
+using ExifUtils.Exif.IO;
 using ExifUtils.Exif.TagValues;
 using ExifUtils.Exif.TypeConverters;
 
