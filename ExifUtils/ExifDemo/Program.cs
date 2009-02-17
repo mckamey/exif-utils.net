e62068ec38ad326365d003db3c8e172a16269364
Stephen McKamey <mckamey@users.noreply.github.com>
2009-02-17 17:59:57 +0000

    - updating licenses

--- ExifUtils/ExifDemo/Program.cs
+++ ExifUtils/ExifDemo/Program.cs
@@ -5,7 +5,7 @@
 
 	The MIT License
 
-	Copyright (c) 2005-2006 Stephen M. McKamey
+	Copyright (c) 2005-2009 Stephen M. McKamey
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
