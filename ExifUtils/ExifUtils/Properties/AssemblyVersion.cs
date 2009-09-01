8426508a4041af095d7ab6300e15ca893d7c7813
Stephen McKamey <mckamey@users.noreply.github.com>
2009-09-01 05:43:51 +0000

    - rev'ing version

--- ExifUtils/ExifUtils/Properties/AssemblyVersion.cs
+++ ExifUtils/ExifUtils/Properties/AssemblyVersion.cs
@@ -2,11 +2,11 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.904.323
+#region 1.0.908.3122
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.904.323")]
-[assembly: AssemblyFileVersion("1.0.904.323")]
+[assembly: AssemblyVersion("1.0.908.3122")]
+[assembly: AssemblyFileVersion("1.0.908.3122")]
 
-#endregion 1.0.904.323
+#endregion 1.0.908.3122
