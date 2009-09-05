e8a639631f4de6d671cdea7794de02ed9c7f5650
smm <smm@e50ce43e-6f37-11de-baec-69240fdc299a>
2009-09-05 17:43:54 +0000

    - updating EXIF orientation names to be more human friendly - adding excellent explanation by Adam M. Costello

--- trunk/ExifUtils/ExifUtils/Properties/AssemblyVersion.cs
+++ trunk/ExifUtils/ExifUtils/Properties/AssemblyVersion.cs
@@ -2,11 +2,11 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.909.323
+#region 1.0.909.510
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.909.323")]
-[assembly: AssemblyFileVersion("1.0.909.323")]
+[assembly: AssemblyVersion("1.0.909.510")]
+[assembly: AssemblyFileVersion("1.0.909.510")]
 
-#endregion 1.0.909.323
+#endregion 1.0.909.510
