79e7084f7ca32e3e3bb548de67d0c22aef8f7438
smm <smm@e50ce43e-6f37-11de-baec-69240fdc299a>
2009-12-21 16:30:34 +0000

    - 1.0.0912.2108

--- trunk/ExifUtils/ExifUtils/Properties/AssemblyVersion.cs
+++ trunk/ExifUtils/ExifUtils/Properties/AssemblyVersion.cs
@@ -2,11 +2,11 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.912.2101
+#region 1.0.912.2108
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.912.2101")]
-[assembly: AssemblyFileVersion("1.0.912.2101")]
+[assembly: AssemblyVersion("1.0.912.2108")]
+[assembly: AssemblyFileVersion("1.0.912.2108")]
 
-#endregion 1.0.912.2101
+#endregion 1.0.912.2108
