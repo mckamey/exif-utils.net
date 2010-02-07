43465e5a151d9920c13f69db913bc64e094ac279
Stephen McKamey <mckamey@users.noreply.github.com>
2010-02-07 04:20:53 +0000

    - v1.0.1002.0620

--- XmpUtils/XmpUtils/Properties/AssemblyVersion.cs
+++ XmpUtils/XmpUtils/Properties/AssemblyVersion.cs
@@ -2,11 +2,11 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.1002.617
+#region 1.0.1002.620
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.1002.617")]
-[assembly: AssemblyFileVersion("1.0.1002.617")]
+[assembly: AssemblyVersion("1.0.1002.620")]
+[assembly: AssemblyFileVersion("1.0.1002.620")]
 
-#endregion 1.0.1002.617
+#endregion 1.0.1002.620
